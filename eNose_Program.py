6818f40bf33a85e4335e036985b4a3662a79dfa1
Ventsislav <hartien4orap@gmail.com>
2025-07-14 10:24:29 +0200

    Add tkinter import for GUI integration

--- eNose_Program.py
+++ eNose_Program.py
@@ -3,6 +3,7 @@ import adafruit_sgp30
 import bme680
 import board
 import adafruit_tca9548a
+import tkinter as tk
 from grove.i2c import Bus
 from rpi_ws281x import PixelStrip, Color
 from grove_ws2813_rgb_led_strip import GroveWS2813RgbStrip
